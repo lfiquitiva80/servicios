b5cee76a39ac5a44f29a9ecfeb476fe977b03f44
Ing. Leonidas Fiquitiva Castro <santifiqui80@hotmail.com>
2018-05-28 14:53:49 -0500

    Cambio 28 de mayo 2018

--- app/ordenesdeservicio.php
+++ app/ordenesdeservicio.php
@@ -8,4 +8,12 @@ class ordenesdeservicio extends Model
 {
      protected $table = 'ordenesdeservicio';
     protected $guarded = ['id'];
+
+
+    public function estadoservicio()
+    {
+        return $this->hasMany('App\ordenesdeservicio', 'estadoservicio_id');
+    }
+
+
 }
